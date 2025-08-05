36592443901d4f75be75e0b22425aaf029681cab
Rajesh Indlamudi <rajesh@gmail.com>
2025-08-05 18:11:20 +0530

    locators

--- pythonProject5/locators/TestLoc.py
+++ pythonProject5/locators/TestLoc.py
@@ -1,6 +1,8 @@
 class Test_loc:
-    def __init__(self,page):
-        self.page =page
+    __test__ = False  # 👈 this tells pytest not to treat it as a test class
+
+    def __init__(self, page):
+        self.page = page
         self.username = page.locator('//*[@name="username"]')
         self.password = page.locator('//*[@name="password"]')
         self.submit = page.locator('//*[@class="wrap-login100-form-btn"]')
--- pythonProject5/test_login.py
+++ pythonProject5/test_login.py
@@ -1,28 +1,17 @@
-import time
+# test_login.py
 
-from playwright.sync_api import sync_playwright
+import pytest
+import time
+from playwright.sync_api import Page
 from locators.TestLoc import Test_loc
 
-
-class Login(Test_loc):
-    def __init__(self,page):
-        super().__init__(page)
-        self.page = page
-        self.loc = Test_loc(page)
-
-    def test_login(self):
-        self.page.goto("http://192.168.1.55:8080/")
-        self.loc.username.fill("admin_vp")
-        self.loc.password.fill("admin_vp")
-        self.loc.submit.dblclick()
-        time.sleep(30)
-
-
-if __name__ == "__main__":
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
-        context = browser.new_context()
-        page = context.new_page()
-        log = Login(page)
-        time.sleep(30)
-        log.test_login()
+@pytest.mark.smoke
+def test_login(page: Page):
+    page.goto("http://192.168.49.2:30080/")  # Replace with your actual Minikube IP + port if dynamic
+    loc = Test_loc(page)
+    loc.username.fill("admin_vp")
+    loc.password.fill("admin_vp")
+    loc.submit.click()
+    
+    time.sleep(3)  # Give the page time to load
+    assert "dashboard" in page.url or page.url != "http://192.168.49.2:30080/"
